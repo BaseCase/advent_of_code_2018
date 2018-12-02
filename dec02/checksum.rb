234b91a1a8bd7cec398a41f5165830d1ccc2a3fe
Casey Brant <casey@caseybrant.com>
2018-12-02 08:01:44 -0600

    Day 02 part 2

--- dec02/checksum.rb
+++ dec02/checksum.rb
@@ -1,3 +1,5 @@
+puts "Starting part 1..."
+
 twos = 0
 threes = 0
 
@@ -8,4 +10,33 @@ File.read('./input.txt').split("\n").each do |line|
   if counts.values.include? 3 then threes += 1 end
 end
 
-puts twos * threes
+puts "Checksum is #{twos * threes}"
+
+
+puts "Starting part 2..."
+input = File.read('./input.txt').split("\n").map { |line| line.split('') }
+
+
+def locate_distance_1_strings(input)
+  (0..input.length - 1).each do |i|
+    (i..input.length - 1).each do |candidate|
+      diffs = 0
+      input[i].each_with_index do |c, idx|
+        if c != input[candidate][idx]
+          diffs += 1
+        end
+      end
+      if diffs == 1
+        a = input[i]
+        b = input[candidate]
+        return [a, b]
+      end
+    end
+  end
+end
+
+a, b = locate_distance_1_strings(input)
+answer = a.select.with_index do |c, idx|
+  b[idx] == c
+end
+puts answer.join
