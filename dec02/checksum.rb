9b2f527e86156c0255ffdab3e922d67016542cf5
Casey Brant <casey@caseybrant.com>
2018-12-02 18:09:22 -0600

    Better Ruby solution for part 2

--- dec02/checksum.rb
+++ dec02/checksum.rb
@@ -12,31 +12,3 @@ end
 
 puts "Checksum is #{twos * threes}"
 
-
-puts "Starting part 2..."
-input = File.read('./input.txt').split("\n").map { |line| line.split('') }
-
-
-def locate_distance_1_strings(input)
-  (0..input.length - 1).each do |i|
-    (i..input.length - 1).each do |candidate|
-      diffs = 0
-      input[i].each_with_index do |c, idx|
-        if c != input[candidate][idx]
-          diffs += 1
-        end
-      end
-      if diffs == 1
-        a = input[i]
-        b = input[candidate]
-        return [a, b]
-      end
-    end
-  end
-end
-
-a, b = locate_distance_1_strings(input)
-answer = a.select.with_index do |c, idx|
-  b[idx] == c
-end
-puts answer.join
