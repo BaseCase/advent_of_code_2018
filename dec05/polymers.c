70eaf5635621592c4f6f0417761d5909fb3f5c63
Casey Brant <casey@caseybrant.com>
2018-12-06 12:23:58 -0600

    Threadify each letter's reduction work.

--- dec05/polymers.c
+++ dec05/polymers.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+
+
+typedef struct reduce_input {
+    char* polymer;
+    int polymer_len;
+    char to_remove;
+    int reduced_length;
+} reduce_input;
 
 
 int is_matched_pair(char a, char b);
+void* reduce_sequence(void* arg);
 
 
 int main(int argc, char** argv)
 {
-    char *original_polymer, *reduced_polymer;
-    int polymer_length, original_reduced_polymer_length, smallest_reduced_polymer_length;
+    char *original_polymer;
+    int polymer_length, smallest_reduced_polymer_length;
+    reduce_input inputs[27];
 
-    //
-    // parse input stream
-    //
     {
         char c;
         int buffer_i;
@@ -28,86 +36,42 @@ int main(int argc, char** argv)
         polymer_length = buffer_i;
     }
 
-
     //
-    // Try the reduction 26 times, for each letter of the alphabet,
-    // removing one letter from the source input at a time.
-    // The answer to part 2 is the length of the shortest of these you can get.
-    // We'll do a run first with no removals to get the answer to part 1.
+    // Try the reduction 27 times: one against the raw input and one for each
+    // letter of the alphabet, removing one letter from the raw input at a
+    // time. The answer to part 1 is the length of the first result. The answer
+    // to part 2 is the length of the shortest of the other runs.
     //
     {
-        int src_i, dest_i, eliminations_count, src_len, reduced_polymer_length;
-        char *src, *dest;
-
-        src = (char*) malloc(sizeof(char) * polymer_length);
-        dest = (char*) malloc(sizeof(char) * polymer_length);
-
-        original_reduced_polymer_length = 0;
-        smallest_reduced_polymer_length = 50000;
+        pthread_t threads[27];
 
         for (int removal_i = -1; removal_i < 26; ++removal_i)
         {
             char skipped_uppercase = removal_i + 'A';
-            char skipped_lowercase = removal_i + 'a';
+            int i = removal_i + 1;
 
-            //
-            // Trim out the unwanted characters before entering the main reduction process.
-            //
-            for (src_i = 0, dest_i = 0; src_i < polymer_length; ++src_i)
-            {
-                if (original_polymer[src_i] == skipped_uppercase || original_polymer[src_i] == skipped_lowercase)
-                    continue;
+            inputs[i].polymer = original_polymer;
+            inputs[i].polymer_len = polymer_length;
+            inputs[i].to_remove = skipped_uppercase;
+            inputs[i].reduced_length = 50000;
 
-                src[dest_i++] = original_polymer[src_i];
-            }
-            src_len = dest_i;
+            pthread_create(&threads[i], NULL, &reduce_sequence, &inputs[i]);
+        }
 
-            //
-            // loop over chars, creating a new array each time and freeing the old one
-            // keep track of how many eliminations occurred
-            // continue looping until you make it to the end with no eliminations
-            //
-            {
-                do {
-                    dest_i = 0;
-                    eliminations_count = 0;
+        for (int i = 0; i < 27; ++i)
+            pthread_join(threads[i], NULL);
 
-                    for (src_i = 0; src_i < src_len; ++src_i)
-                    {
-                        if ((src_i != src_len - 1)  // don't need to check pairs if we're on the last char
-                                && is_matched_pair(src[src_i], src[src_i + 1]))
-                        {
-                            // jump over pairs while copying the string
-                            ++eliminations_count;
-                            ++src_i;
-                            continue;
-                        }
-
-
-                        dest[dest_i++] = src[src_i];
-                    }
-                    dest[dest_i] = 0;
-
-                    src = dest;
-                    src_len = dest_i;
-
-                } while (eliminations_count > 0);
-
-                reduced_polymer = src;
-                reduced_polymer_length = src_len;
-
-                if (original_reduced_polymer_length == 0)
-                    original_reduced_polymer_length = reduced_polymer_length;
-
-                if ((reduced_polymer_length < smallest_reduced_polymer_length) && (reduced_polymer_length > 0))
-                    smallest_reduced_polymer_length = reduced_polymer_length;
-            }
-
+        smallest_reduced_polymer_length = 50000;
+        for (int i = 0; i < 27; ++i)
+        {
+            int len = inputs[i].reduced_length;
+            if (len < smallest_reduced_polymer_length)
+                smallest_reduced_polymer_length = len;
         }
     }
 
-
-    printf("The full reduced polymer is length %d\n", original_reduced_polymer_length);
+    // We know that the first input didn't remove any letters, so it's the answer to part 1.
+    printf("The full reduced polymer is length %d\n", inputs[0].reduced_length);
     printf("The smallest reduced polymer when stripping one letter at a time is length %d\n", smallest_reduced_polymer_length);
 }
 
@@ -118,4 +82,70 @@ int is_matched_pair(char a, char b)
         return (a == (b + 32));
     else
         return (b == (a + 32));
+}
+
+
+void* reduce_sequence(void* arg)
+{
+    reduce_input* input = (reduce_input*)arg;
+    char* original_polymer = input->polymer;
+    int polymer_length = input->polymer_len;
+    char uppercase_skip = input->to_remove;
+    char lowercase_skip = uppercase_skip + 32;
+
+    int src_i, dest_i, eliminations_count, src_len, reduced_polymer_length;
+    char *src, *dest;
+
+    src = (char*) malloc(sizeof(char) * polymer_length);
+    dest = (char*) malloc(sizeof(char) * polymer_length);
+
+    int smallest_reduced_polymer_length = 50000;
+
+    //
+    // Copy the source string and trim out the unwanted characters before entering the main reduction process.
+    //
+    for (src_i = 0, dest_i = 0; src_i < polymer_length; ++src_i)
+    {
+        if (original_polymer[src_i] == uppercase_skip || original_polymer[src_i] == lowercase_skip)
+            continue;
+
+        src[dest_i++] = original_polymer[src_i];
+    }
+    src_len = dest_i;
+
+    //
+    // loop over chars, keep track of how many eliminations occurred
+    // continue looping until you make it to the end with no eliminations
+    //
+    do {
+        dest_i = 0;
+        eliminations_count = 0;
+
+        for (src_i = 0; src_i < src_len; ++src_i)
+        {
+            if ((src_i != src_len - 1)  // don't need to check pairs if we're on the last char
+                    && is_matched_pair(src[src_i], src[src_i + 1]))
+            {
+                // jump over pairs while copying the string
+                ++eliminations_count;
+                ++src_i;
+                continue;
+            }
+
+            dest[dest_i++] = src[src_i];
+        }
+        dest[dest_i] = 0;
+        src = dest;
+        src_len = dest_i;
+
+    } while (eliminations_count > 0);
+
+    reduced_polymer_length = src_len;
+
+    if ((reduced_polymer_length < smallest_reduced_polymer_length) && (reduced_polymer_length > 0))
+        smallest_reduced_polymer_length = reduced_polymer_length;
+
+    input->reduced_length = smallest_reduced_polymer_length;
+
+    return NULL;
 }
